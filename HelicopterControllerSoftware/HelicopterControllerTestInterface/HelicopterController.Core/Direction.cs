363af92ea33682192b9805c25168c2a95a90290d
Aftab Narsimhan <aftabn94@gmail.com>
2016-02-06 01:07:57 -0800

    Added enums to match hardware/firmware implementation

---
+++ HelicopterControllerSoftware/HelicopterControllerTestInterface/HelicopterController.Core/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelicopterController.Core
+{
+    public enum Direction
+    {
+        Clockwise, CounterClockwise
+    }
+}
