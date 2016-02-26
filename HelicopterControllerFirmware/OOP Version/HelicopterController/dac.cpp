bccd11352b3a931d45aab695b89b983b1facda53
Aftab Narsimhan <aftabn94@gmail.com>
2016-02-25 21:28:16 -0800

    Fleshed out most of Dac class

--- HelicopterControllerFirmware/OOP Version/HelicopterController/dac.cpp
+++ HelicopterControllerFirmware/OOP Version/HelicopterController/dac.cpp
@@ -1,7 +1,58 @@
-// 
-// 
-// 
+//
+//
+//
 
 #include "dac.h"
 
+const double Dac::DBL_DefaultVoltage = 2.5;
+const double Dac::DBL_ReferenceVoltage = Utility::DBL_ArduinoVss;
+const double Dac::DBL_VoltsPerBit = DBL_ReferenceVoltage / Dac::INT_Resolution;
 
+void Dac::initialize()
+{
+	pinModeFast(Utility::PIN_DacChipSelect, OUTPUT);
+	digitalWriteFast(Utility::PIN_DacChipSelect, LOW);
+	digitalWriteFast(Utility::PIN_DacChipSelect, HIGH);
+
+	for (int channel = 0; channel < Utility::INT_MaxNumChannels; channel++)
+	{
+		setDacVoltage(channel, DBL_DefaultVoltage);
+	}
+}
+
+int Dac::convertVoltageToDacValue(double voltage)
+{
+	int value = (int)(voltage / DBL_ReferenceVoltage * INT_Resolution);
+	return value;
+}
+
+// TODO: Refactor this once requirements and sensors have been finalized
+// Refer to : https://github.com/wigman27/Tutorial-Using-Arduino-SPI for more info
+void Dac::setDacVoltage(int channel, double voltage)
+{
+	int value = convertVoltageToDacValue(voltage);
+
+	// Bit Significance: 1st = channel, 2nd = bypasses input buffer, 3rd = output gain x1, 4th = active low SHDN, LSBs don't matter
+	int defaultDacRegister = 0B00110000;					//
+	int secondByteMask = 0b0000000011111111;				// For selecting last 8 bits
+	byte primaryByte = (value >> 6) | defaultDacRegister;	// Isolates the 2 MSB for ORing
+	byte secondaryByte = (value << 2) & secondByteMask;		// Isolates the 8 LSB
+
+															// Sets the MSB in the primaryByte to determine the DAC to be set, DAC A=0, DAC B=1
+	switch (channel) {
+	case 0:
+		primaryByte &= ~(1 << 7);
+		break;
+	case 1:
+		primaryByte |= (1 << 7);
+	}
+
+	noInterrupts();
+	digitalWriteFast(Utility::PIN_DacChipSelect, LOW);
+	SPI.transfer(primaryByte);
+	SPI.transfer(secondaryByte);
+	digitalWriteFast(Utility::PIN_DacChipSelect, HIGH);
+	interrupts();
+
+	//currentVoltages[channel] = voltage;
+}
--- HelicopterControllerFirmware/OOP Version/HelicopterController/dac.h
+++ HelicopterControllerFirmware/OOP Version/HelicopterController/dac.h
@@ -3,12 +3,20 @@
 #ifndef _DAC_h
 #define _DAC_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
-#else
-	#include "WProgram.h"
+class Dac
+{
+private:
+	static const int INT_Resolution = 1023;
+	static const double DBL_ReferenceVoltage;
+	static const double DBL_VoltsPerBit;
+	static const double DBL_DefaultVoltage;
+
+	int convertVoltageToDacValue(double voltage);
+
+public:
+	Dac() {}
+	void initialize(void);
+	void setDacVoltage(int channel, double voltage);
+};
+
 #endif
-
-
-#endif
-
