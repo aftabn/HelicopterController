29a188e07b0127f6a443266ed0d4fc42d454d63a
Wyatt Gronnemose <w.gron94@gmail.com>
2016-04-04 23:44:22 -0700

    Created last minute rework versions of system model

---
+++ System Model/Last minute rework/constants.m
@@ -0,0 +1,71 @@
+% ELEC 391 Project: Control System's Group
+% AUTHORS: Wyatt & Aftab
+
+% This script contains physical constants and specifications
+
+% Physical Constants
+G = 9.81;               % m/s^2
+FtPerM = 3.28;          % ft/m
+RadPerDeg = pi/180;     % rad/deg
+DegPerRad = 180/pi;     % deg/rad
+Kgm2Pergcm2 = 1e-7;     % Kgm^2/gcm^2
+RPMPerw = 30/pi;        % RPM / rad/s
+
+% Desired Trajectory
+% Row 1 = value
+% Row 2 = time
+Alt = [ 00  00  40    40
+        00  15  15.1  30];
+Yaw = [ 00  90
+        00  30];
+
+% Mechanical Properties
+% The length of the arm going to the main motor (m).
+% Measured from the fulcrum to the end
+mainArmLength = 0.15;
+% The length of the arm going to the yaw motor (m)
+% Measured from the fulcrum to the end
+yawArmLength = 0.15;
+
+% Main Motor
+Lmm  = 1.2E-3;         % Inductance     (H)
+Rmm  = 16;             % Resistance     (Ohms)
+Kbmm = 586;            % Back EMF       (RPM/V)
+Ktmm = 60/(2*pi*Kbmm); % Torque Gain    (Nm/A)
+Jmm  = 0;              % Inertia        (kg m^2)
+Bmm  = Ktmm/(Kbmm*Rmm)*RPMPerw;              % Friction       (kg m^2/s)
+Mmm  = 170E-3;         % Mass (with bracket)    (kg)
+mainBracketMass = 0;
+
+% Main Rotor
+Nmr  = 2;           % Number of Blades
+Lmr  = 0.133;       % Length of a single blade (m)
+Mmr  = 5E-3;        % Mass (total)   (kg)
+Fmr  = 0;           % Friction       (kgm^2/s)
+TPmr = 2.9E-6;      % Thrust Axial   (kg/RPM)
+
+% Tail Motor
+Ltm  = Lmm;           % Inductance     (H)
+Rtm  = Rmm;           % Resistance     (Ohms)
+Kttm = Ktmm;           % Torque Gain    (Nm/A)
+Kbtm = Kbmm;           % Back EMF       (RPM/V)
+Jtm  = Jmm;           % Inertia        (kgm^2)
+Btm  = Bmm;           % Friction       (kgm^2/s)
+Mtm  = Mmm;           % Mass           (kg)
+tailBracketMass = (6 + 2*22)*10^-3;
+
+% Tail Rotor
+Ntr  = 2;             % Number of Blades
+Ltr  = Lmr;           % Length         (m)
+Mtr  = Mmr;           % Mass           (kg)
+Ftr  = Fmr;           % Friction       (kgm^2/s)
+TPtr = TPmr;          % Thrust Axial   (kg/RPM)
+
+% Drone
+B1h = 0;            % Altitude Rotational Resistance (kgm^2/s)
+J1h  = 0.002;           % Altitude Mom of Inertia (kgm^2)
+J2h  = 0.009;           % Yaw Mom of Inertia (kgm^2)
+B2h = 0;            % Yaw Rotational resistance      (kgm^2/s)
+
+% Circuits
+Vss = 5;            % V
---
+++ System Model/Last minute rework/setup.m
@@ -0,0 +1,113 @@
+% ELEC 391 Project: Control System's Group
+% AUTHORS: Wyatt & Aftab
+
+% This script sets all model parameters for the ELEC 391 helicopter control project
+
+clear all;  % Erase everything from memory
+constants;  % Assign physical constants
+
+% Example:
+% For Dynamics Blocks
+% N = [1 2 3];
+% D = [4 5 6];
+% Means:
+% Transfer Function = (s^2 + 2s + 3) / (4s^2 + 5s + 6)
+% All Dynamics blocks are initialized to N/D = 1/1 = 1
+
+% ======================
+% CONTROLLER & AMPLIFIER
+% ======================
+% PID Controller
+mPID = [0, 0, 0];
+tPID = [0.01,0,0.1];
+
+% Bias
+CurrentDriverBias = Vss/2;
+
+% Current driver gain
+% Max voltage for the motor is 12V. The output of the computer is 0-5V where 2.5V
+% is 0 velocity. So 2.5/12=4.8
+ControlDriverGain = 4.8;
+
+% Min and Max height
+% This is determined by physical hardstops
+MaxHeight = 0.05; %EXAMPLE VALUES - CHANGE THESE
+MinHeight = -0.05; 
+
+% Saturation
+mSat = Vss/2;
+tSat = Vss/2;
+
+% =====
+% MOTOR
+% =====
+% Elec Dynamics (A/V)
+mElecDynNum = [1];
+mElecDynDen = [Lmm Rmm];
+tElecDynNum = [1];
+tElecDynDen = [Ltm Rtm];
+
+% Torque Constant (Nm/A)
+mTConst = Ktmm;
+tTConst = Kttm;
+
+% Mech Dynamics (RPM/Nm)
+% Must compute rotor dynamics from available constants
+Jmr = 1/12 * (2*Lmr)^2 * Mmr;
+Bmr = Fmr * Nmr;
+mMechDynNum = [RPMPerw];
+mMechDynDen = [Jmm+Jmr Bmm+Bmr];
+
+Jtr = Jmr;
+Btr = Bmr;
+tMechDynNum = [RPMPerw];
+tMechDynDen = [Jtm+Jtr Btm+Btr];
+
+% Rotor Drag Torque (Nm/RPM)
+% (gcm2/s) * (Kgm2/gcm2) / (RPM / rad/s) = (Kgm^2/s^2)/RPM = Nm/RPM
+mDrag = Fmr * Kgm2Pergcm2 / RPMPerw * Nmr;
+
+% Back EMF (V/RPM)
+mBackEMF = 1/Kbmm;
+tBackEMF = 1/Kbtm;
+          
+% ==========
+% HELICOPTER
+% ==========
+% Gravity (N)
+% Include motors, gear, rotors, and heli
+%M = (Mmm + Mmg + Mmr*Nmr + Mtm + Mtr*Ntr + Mh) * 1e-3;   % Kg
+%Grav = M*G;                   
+
+% Altitude Helicopter Dynamics:
+% Heli Lift (Nm/RPM)
+mLiftTorque   = TPmr * G * mainArmLength; % Kgm^2/s^2 / RPM  = Nm/RPM
+torqueFromYawMotorMass = (tailBracketMass + Mtm) * G * yawArmLength;
+torqueFromAltMotorMass = (mainBracketMass + Mmm) * G * mainArmLength;
+
+mHeliDynNum = DegPerRad;
+mHeliDynDen = [J1h B1h 0];
+altSaturation = 45;
+
+% Yaw Helicopter Dynamics:
+% Tail Torque (Nm/RPM)
+tTorque = TPtr * G * yawArmLength;  % Kgm/s^2 / RPM * m = Nm/RPM
+
+tHeliDynNum = DegPerRad; 
+tHeliDynDen = [J2h B2h 0];   % Rotational Inertia / Air R
+yawSaturation = 180;
+
+% =======
+% Sensors
+% =======
+% Altitude ADC:
+ADC_bitwidth = 10;
+ground = 0;
+
+% Altitude Potentiometer
+angleRange = 145; % Potentiometer can turn +/- 145 degrees
+potentiometerGain = Vss/(2*angleRange);
+potentiometerBias = Vss/2; % It is mounted so that is outputs 2.5 V
+                           % when the angle is 0
+% Yaw encoder
+yawEncoderQuantization = 360/400; %360 degrees/400 pulses per revolution
